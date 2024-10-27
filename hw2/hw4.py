3aed68841031abc9b97c10a7c821b5abfe95079f
Daria Milyakova <103563978+GithubAgitator@users.noreply.github.com>
2024-10-27 14:42:32 +0300

    hw4

--- hw2/hw4.py
+++ hw2/hw4.py
@@ -1,29 +1,11 @@
 lst = ['Hello', 1, '7', True, False, 5.4, (1,)]
-d = {'int': [], 'str': [], 'float': [], 'bool': [], 'tuple': []}
-strs = []
-ints = []
-floats = []
-bools = []
-tuples = []
+d = {}
 for i in lst:
-    if type(i) == str:
-        strs.append(i)
-        d['str'] = strs
-    elif type(i) == int:
-        ints.append(i)
-        d['int'] = ints
-    elif type(i) == float:
-        floats.append(i)
-        d['float'] = floats
-    elif type(i) == bool:
-        bools.append(i)
-        d['bool'] = bools
-    elif type(i) == tuple:
-        tuples.append(i)
-        d['tuple'] = tuples
+    k = type(i)
+    if k not in d:
+        d[k] = []
+    d[k].append(i)
 print(d)
-dicts = {k: v for k, v in d.items() if v}
-print(dicts)
 
 
 
